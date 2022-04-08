e93d2f49ef98ff10fc61d87269c0aef1fb095677
曾小健 <2119516028@qq.com>
2022-04-08 15:56:52 +0800

    Create leetcode_practice.py

---
+++ leetcode_practice.py
@@ -0,0 +1,9 @@
+1.twoSum
+class Soluton:
+  def twoSum(self, nums:List[int], target:int) -> List[int]:
+    n = len(nums)
+    for i in range(n):
+      for j in range(i+1,n):
+        if nums[i]+nums[j] == target:
+          return [i,j]
+    return []
