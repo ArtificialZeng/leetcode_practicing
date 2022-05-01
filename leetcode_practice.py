144ab7f43f9457e27312216f79cc52e118550ff6
曾小健 <2119516028@qq.com>
2022-05-02 02:04:19 +0800

    Update leetcode_practice.py

--- leetcode_practice.py
+++ leetcode_practice.py
@@ -8,7 +8,30 @@ class Soluton:
           return [i,j]
     return []
 
-  
+
+66.class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                for j in range(i + 1, n):
+                    digits[j] = 0
+                return digits
+class Solution:
+  def plusOne(self,digits:List[int]) -> List[int]:
+    n = len(digits)
+    for i in range(n-1,-1,-1):
+       if digits[i]!=9:
+          digits[i] +=1
+          for j in range(i+1,n):
+            digits[j] =0
+           return digits
+    return [1]+[0] *n
+
+        # digits 中所有的元素均为 9
+        return [1] + [0] * n
+
   addOne
 class Solution:
   def addOne(self,List[int]) ->
